9a526ba96c1425ad9adc9cc80eb608064d522294
Gabe Conradi <gabe@tumblr.com>
2015-07-12 16:55:21 -0400

    refactor stuff (not working yet)

---
+++ handlers/asset_tag.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"github.com/nlopes/slack"
+)
+
+func (c *Context) AssetTagHandler(m *slack.MessageEvent) (bool, error) {
+	// dont process if this came from a bot (like ourselves). avoids looping
+	if isBotMessage(m) {
+		return false, nil
+	}
+	// handle messages with any asset tags present - we will turn them into collins links
+	tags := extractAssetTags(m.Msg.Text)
+	if len(tags) > 0 {
+		assets := c.lookupAssetsFromTags(tags)
+		// send a message back to that channel with the links to the assets
+		p := slack.NewPostMessageParameters()
+		p.Attachments = make([]slack.Attachment, len(assets))
+		for i, asset := range assets {
+			p.Attachments[i] = c.slackAssetAttachment(asset)
+		}
+		_, _, err := c.Slack.PostMessage(m.ChannelId, "", p)
+		return true, err
+	}
+	return false, nil
+}
---
+++ handlers/you_alive.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/nlopes/slack"
+	"log"
+	"strings"
+)
+
+func (c *Context) YouAliveHandler(m *slack.MessageEvent) (bool, error) {
+	matched := c.isBotMention(m) && strings.Contains(m.Msg.Text, "yt?")
+	if matched {
+		log.Printf("Got yt? message %+v", m.Msg)
+		u, err := c.Slack.GetUserInfo(m.Msg.UserId)
+		if err != nil {
+			return false, err
+		}
+		//_, _, err = api.PostMessage(m.ChannelId, fmt.Sprintf("Not dead yet, @%s", u.Name), postParams)
+		c.chOutgoingMessages <- *c.ws.NewOutgoingMessage(fmt.Sprintf("Not dead yet, <@%s|%s>", m.Msg.UserId, u.Name), m.ChannelId)
+		return true, err
+	}
+	return false, nil
+}
